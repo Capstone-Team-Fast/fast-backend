58ac158d274e7bfdcf32f53297191909ff08e3a5
Isidore Jeovanis Odilon Sossa <isossa@unomaha.edu>
2021-09-28 19:13:01 -0500

    Setup - Added client, location, and route models.

--- backend/models/client.py
+++ backend/models/client.py
@@ -2,7 +2,15 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+# https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#extending-the-existing-user-model
+# more stuff here
+from backend.models import Route, Language
+
+
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#extending-the-existing-user-model
-    # more stuff here
+    quantity = models.PositiveIntegerField(null=False)
+    route = models.ManyToManyField(to=Route)
+    languages = models.ManyToManyField(to=Language)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_on = models.DateTimeField(auto_now=True, editable=False)
--- backend/models/route.py
+++ backend/models/route.py
@@ -1,5 +1,12 @@
 from django.db import models
 
+from backend.models import Driver
+
 
 class Route(models.Model):
-    date = models.DateField()
+    assigned_to = models.ForeignKey(Driver, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
+    modified_on = models.DateTimeField(auto_now=True, editable=False)
+    total_quantity = models.PositiveIntegerField(editable=False)
+    total_distance = models.DecimalField(max_digits=19, decimal_places=4, editable=False)
+    total_duration = models.DecimalField(max_digits=19, decimal_places=4, editable=False)
