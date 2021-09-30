419f424659dbbc9807a7740efed1c58acb912fe4
Liam <lbetterman@gmail.com>
2021-09-29 21:43:02 -0500

    Added User (Django default, built-in), Driver, and Manager models

--- backend/models/manager.py
+++ backend/models/manager.py
@@ -4,5 +4,7 @@ from django.db import models
 
 class Manager(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=15, blank=True)
+    
     # https://docs.djangoproject.com/en/3.2/topics/auth/customizing/#extending-the-existing-user-model
     # more stuff here
