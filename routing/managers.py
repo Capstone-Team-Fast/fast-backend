c732b88468d9be9f0684bcfd49c7dd21a893683b
isossa <sossajeovanis@gmail.com>
2021-10-03 21:03:14 -0500

    [M1001] - Added LocationManager.

--- routing/managers.py
+++ routing/managers.py
@@ -1,3 +1,5 @@
+import neomodel
+
 from routing.models.location import Location
 
 
@@ -6,4 +8,26 @@ class DriverManager:
 
 
 class LocationManager:
-    locations = set(Location)
+
+    def __init__(self, db_connection: neomodel.db):
+        self.locations = set()
+        self.connection = db_connection
+        self.__set_locations()
+
+    def __set_locations(self):
+        for location in Location.nodes.all():
+            self.add(location)
+
+    def delete(self, location: Location):
+        if not isinstance(location, Location):
+            return NotImplemented
+        self.locations.remove(location)
+
+    def get_locations(self):
+        return self.locations
+
+    def add(self, location: Location):
+        if not isinstance(location, Location):
+            return NotImplemented
+        self.locations.add(location)
+
