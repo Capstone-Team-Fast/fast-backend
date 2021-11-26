af22dc64fdafb5bc6bea0fdf91eb906378e89f81
SkootNoog <scottlnegus@gmail.com>
2021-11-26 09:38:45 -0600

    route serializer nested serializer update

--- backend/serializers/routeSerializer.py
+++ backend/serializers/routeSerializer.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
 from backend.models import Route
+from models import Driver, Client
+from serializers import DriverSerializer, ClientSerializer
 
 
 class RouteSerializer(serializers.ModelSerializer):
+    assigned_to = DriverSerializer
+    itinerary = ClientSerializer(many=True)
 
     def create(self, validated_data):
+        assigned_to_data = validated_data.pop('assigned_to')
+        itinerary_data = validated_data.pop('itinerary')
+
+        assigned_to = Driver.objects.get_or_create(**assigned_to_data)
+
+        for i_data in itinerary_data:
+            itinerary = Client.objects.get_or_create(**i_data)
+
         return Route.objects.create(**validated_data)
 
     class Meta:
