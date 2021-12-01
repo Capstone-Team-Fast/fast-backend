25f0b4ef12c726703a838146bff405c809733ba2
SkootNoog <scottlnegus@gmail.com>
2021-12-01 15:52:56 -0600

    testing saving itinerary

--- backend/serializers/routeSerializer.py
+++ backend/serializers/routeSerializer.py
@@ -13,12 +13,15 @@ class RouteSerializer(serializers.ModelSerializer):
 
         assigned_to = Driver.objects.get_or_create(**assigned_to_data)
 
+        route = Route.objects.create(**validated_data)
+
         for i_data in itinerary_data:
             if i_data.get('is_center') == False or i_data.get('is_center') == 'false':
                 i_id = i_data.get('id')
-                itinerary = Client.objects.get_or_create(id=i_id)
+                itinerary = Client.objects.get(id=i_id)
+                route.itinerary.add(itinerary)
 
-        return Route.objects.create(**validated_data)
+        return route
 
     class Meta:
         model = Route
